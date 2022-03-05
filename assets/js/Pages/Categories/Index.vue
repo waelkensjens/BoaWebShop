745939c58c96168a1aba86da0cdcda85d172c627
jens <waelkens.jens@gmail.com>
2022-03-05 21:44:38 +0100

    :lipstick: update header

--- assets/js/Pages/Categories/Index.vue
+++ assets/js/Pages/Categories/Index.vue
@@ -7,27 +7,11 @@
         <div
             v-for="product in category.products"
             class="group relative">
-          <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-            <img :src="image"
-                 class="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                 alt="beer-image">
-          </div>
-          <div class="mt-4 flex justify-between">
-            <div>
-              <h3 class="text-sm text-gray-700">
-                <a href="#">
-                  <span aria-hidden="true" class="absolute inset-0"></span>
-                  Basic Tee
-                </a>
-              </h3>
-              <p class="mt-1 text-sm text-gray-500">Black</p>
-            </div>
-            <p class="text-sm font-medium text-gray-900">$35</p>
-          </div>
+          <Product
+            :product="product"
+          />
         </div>
-        <!-- More products... -->
       </div>
-
     </div>
   </div>
 
@@ -35,7 +19,6 @@
 
 <script>
 import Layout from "@/Shared/Layout";
-import image from '../../../images/beer.jpg'
 
 export default {
   name: "Index",
@@ -43,6 +26,9 @@ export default {
 }
 </script>
 <script setup>
+import image from '../../../images/beer.jpg'
+import Product from '@/Shared/Product'
+
 
 import {computed} from "vue";
 
--- assets/js/Shared/Header.vue
+++ assets/js/Shared/Header.vue
@@ -2,13 +2,19 @@
 
   <nav class="flex justify-between bg-gray-50 text-gray-700 border border-gray-200 py-3 px-5 rounded-lg dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
     <div class="flex items-center py-4 overflow-y-auto whitespace-nowrap">
+      <div class="">
       <Link :href="route('home')"
-            class="text-gray-600 dark:text-gray-200"
+            class="flex flex-inline text-pink-500 dark:text-gray-200"
       >
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
           <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
         </svg>
+        <span class="text-pink-500 font-black uppercase px-2">
+          Beerz
+        </span>
       </Link>
+
+      </div>
 
       <span class="mx-5 text-gray-500 dark:text-gray-300">
             /
@@ -16,7 +22,15 @@
 
 
       <div v-for="(category, index) in $page.props.categories">
-        <Link class="text-gray-600 dark:text-gray-200 hover:underline"
+        <Link
+            class="text-gray-600
+            dark:text-gray-200
+            font-black
+            uppercase
+            px-2
+            hover:text-pink-500
+            hover:underline-offset-8
+            active:text-pink-500"
               :href="route('categories.show', {name: category.name})"
         >
           {{category.name}}
@@ -28,21 +42,37 @@
         </span>
       </div>
     </div>
-    <div class="flex justify-end">
-      <a
+    <div class="flex flex-inline justify-end">
+      <a class="relative inline-block hover:text-pink-500"
           href="">
-        <i class="las la-shopping-cart w-28">cart</i>
-        <img src="https://img.icons8.com/material-outlined/24/000000/shopping-cart--v1.png"/>
+        <i class="las la-shopping-cart la-2x ">
+
+        </i>
+        <span
+            v-if="true"
+            class="
+            absolute
+            -top-2
+            -left-4
+            p-1
+            text-xs
+            text-white
+            bg-pink-500
+            rounded-full"
+        >
+          99
+        </span>
+        <span class="">cart</span>
       </a>
+
     </div>
   </nav>
+
 
 </template>
 
 <script setup>
-import Routing from '@/Routing'
 import { Link } from '@inertiajs/inertia-vue3'
-import { usePage } from '@inertiajs/inertia-vue3'
 
 const props = defineProps({
   categories: Object
